95054ac7784d9586bdab4aa70f793208883d1636
hamilton <hamilton.ulmer@gmail.com>
2020-06-23 07:20:23 -0700

    adds back querystring state loading

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,32 @@
 <script>
+  import { onMount } from 'svelte';
   import Sidebar from "./components/Sidebar.svelte";
   import Explore from "./components/Explore.svelte";
   import LoadingSpinner from './components/LoadingSpinner.svelte';
   import ErrorMessage from './components/ErrorMessage.svelte';
-  import { createRequestCache } from './stores/cache';
+  import { createRequestCache, storeToQuery } from './stores/cache';
   import { store } from './stores/store';
 
   const cache = createRequestCache();
+
+  function updateQueryString(value) {
+    if (history.pushState) {
+      const newurl =
+        window.location.protocol +
+        "//" +
+        window.location.host +
+        window.location.pathname +
+        `?${value}`;
+      window.history.pushState({ path: newurl }, "", newurl);
+    }
+  }
+
+  let mounted = false;
+  onMount(() => { mounted = true });
+
+  $: if (mounted) {
+    updateQueryString(storeToQuery($store));
+  }
 
 </script>
 
